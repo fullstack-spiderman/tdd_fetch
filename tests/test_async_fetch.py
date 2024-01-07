884e103498e6946be8a9f642e94dd731ecdd934a
Arjun KayalMoni <arjun.umathanu@gmail.com>
2024-01-07 21:25:20 +0530

    Refactor test code

--- tests/test_async_fetch.py
+++ tests/test_async_fetch.py
@@ -1,5 +1,6 @@
 '''test module for test_async_find_album_by_id'''
 from typing import Any
+from httpx import Response
 
 import pytest
 from pytest_httpx import HTTPXMock
@@ -7,18 +8,45 @@ from pytest_httpx import HTTPXMock
 from app.async_fetch import async_find_album_by_id
 
 
-pytestmark: pytest.MarkDecorator = pytest.mark.anyio
-
-
+@pytest.mark.asyncio
 async def test_async_find_album_by_id(httpx_mock: HTTPXMock) -> None:
-    '''test code for test_async_find_album_by_id'''
+    '''test code for async_find_album_by_id'''
+
+    mocked_response: dict[str, int|str] = {
+            "userId": 2,
+            "id": 2,
+            "title": "spiderman"
+        }
     httpx_mock.add_response(
         method="GET",
-        url='https://jsonplaceholder.typicode.com/albums/1', json={
-            "userId": 1,
-            "id": 1,
-            "title": "enim"
-        })
+        url='https://jsonplaceholder.typicode.com/albums/1', json=mocked_response,
+    )
 
-    result: dict[str, Any] | None = await async_find_album_by_id(1)
-    assert result == 'enim'
+    response: Response = await async_find_album_by_id(1)
+
+    assert response.status_code == 200
+    assert response.json() == mocked_response
+
+# @pytest.mark.anyio
+# async def test_async_find_all_albums(httpx_mock: HTTPXMock) -> None:
+#     '''test code for async_find_all_albums'''
+#     mocked_response: list[dict[str, int|str]] = [
+#         {
+#             "userId": 1,
+#             "id": 1,
+#             "title": "ironman"
+#         },
+#         {
+#             "userId": 2,
+#             "id": 2,
+#             "title": "spiderman"
+#         }
+#     ]
+#     httpx_mock.add_response(
+#         method="GET",
+#         url='https://jsonplaceholder.typicode.com/albums/', json=mocked_response)
+
+#     response: dict[str, Any] | None = await async_find_all_albums()
+
+#     assert response.status_code == 200
+#     assert response.json() == mocked_response
